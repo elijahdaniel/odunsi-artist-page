7a2dc9270b98f18badcf00be763598277f9a2b45
Elijah Paminsan <e.paminsan@gmail.com>
2020-01-20 22:38:37 -0800

    header and banner components
    styled both components and adding info into both sections

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Header = () => (
   <header>
-    <p>header</p>
+    <div className="container">
+      <div className="inner-header">
+        <div className="logo">
+          <Link to="/">ODUNSI</Link>
+        </div>
+        <div className="navigation">
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/work">Work</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </div>
+      </div>
+    </div>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 //scss
 import "../styles/styles.scss"
 // Normalize is optional up to you
 import "normalize.css"
+// components
+import Header from "../components/header"
+import Banner from "../components/banner"
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi from the home page</h1>
-    <Link to="/page-2">Go to page 2</Link>
-  </div>
-)
+const IndexPage = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  )
+}
 
 export default IndexPage
